34c117ee6afcd11c4874a2103ab2793c9e2b2d42
Kamil Kowalewski <szamil24@gmail.com>
2021-03-10 21:15:23 +0100

    Add data + criteria.py

---
+++ Task1/module/decision_criteria/criteria.py
@@ -0,0 +1,23 @@
+# MIni Max - Wald
+def mini_max_criteria() -> None:
+    pass
+
+
+# Max Max
+def max_max_criteria() -> None:
+    pass
+
+
+# Hurwicz
+def hurwicz_criteria() -> None:
+    pass
+
+
+# Bayes Laplace
+def bayes_laplace_criteria() -> None:
+    pass
+
+
+# Savege
+def savage_criteria() -> None:
+    pass
