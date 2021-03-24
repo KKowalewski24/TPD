e4a66ce1cf8541b6d29eeb696e4703092f027de9
Kamil Kowalewski <szamil24@gmail.com>
2021-03-24 18:55:26 +0100

    Add partially done reduce_rows_cols_in_matrix

--- Task2/module/functions.py
+++ Task2/module/functions.py
@@ -30,5 +30,26 @@ def reduce_rows_cols_in_matrix(primary_matrix: np.ndarray) -> np.ndarray:
     cols_number = primary_matrix.shape[1]
 
     reduced_matrix: np.ndarray = np.ndarray([])
+    for i in range(rows_number):
+        for j in range(rows_number):
+            if i != j:
+                if (primary_matrix[i] <= primary_matrix[j]).all() \
+                        and is_dominated_row(primary_matrix[i], primary_matrix[j]):
+                    print(primary_matrix[i])
+                    print()
 
     return reduced_matrix
+
+
+def is_dominated_row(row_to_check: np.ndarray, other_row: np.ndarray) -> bool:
+    if len(row_to_check) != len(other_row):
+        raise Exception("Lists must have equal length")
+
+    return (row_to_check < other_row).any()
+
+
+def is_dominated_column(column_to_check: np.ndarray, other_column: np.ndarray) -> bool:
+    if len(column_to_check) != len(other_column):
+        raise Exception("Lists must have equal length")
+
+    return (column_to_check < other_column).any()
