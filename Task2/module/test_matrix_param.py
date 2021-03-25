21b3af74b5bf543f35f2db2538db2402c85876aa
Kamil Kowalewski <szamil24@gmail.com>
2021-03-25 20:25:58 +0100

    Add check_if_has_average_win

--- Task2/module/test_matrix_param.py
+++ Task2/module/test_matrix_param.py
@@ -6,10 +6,11 @@ from module.functions import has_saddle_point, max_min_by_rows, min_max_by_colum
 from module.matrix_reducer import reduce_rows_cols_in_matrix
 
 
-def test_different_matrix_param(primary_matrix: np.ndarray,
-                                range_begin: int, range_end: int) -> None:
+def test_different_matrix_param(primary_matrix: np.ndarray, range_begin: int,
+                                range_end: int, average_win: int) -> None:
     display_params(test_saddle_point(primary_matrix, range_begin, range_end))
     display_params(test_mixed_strategies(primary_matrix, range_begin, range_end))
+    # TODO ADD USAGE OF check_if_has_average_win
 
 
 def test_saddle_point(primary_matrix: np.ndarray,
@@ -37,6 +38,16 @@ def test_mixed_strategies(primary_matrix: np.ndarray,
         # TODO ADD NEXT STEPS
 
     return param_values_for_mixed_strategies
+
+
+# '-' near value means opposite value
+# Checking opposite value is required because this is game with zero sum
+def check_if_has_average_win(player_a_value: int, player_b_value: int,
+                             average_win: int) -> bool:
+    if (player_a_value == average_win and player_b_value == - average_win) \
+            or (player_b_value == average_win and player_a_value == -average_win):
+        return True
+    return False
 
 
 def display_params(params: List[int]) -> None:
