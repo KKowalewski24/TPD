1861c090af91a398f3dc4aa5c9d234e509637bb1
Kamil Kowalewski <szamil24@gmail.com>
2021-03-27 19:04:53 +0100

    Rename primary_matrix

--- Task2/module/matrix_reducer.py
+++ Task2/module/matrix_reducer.py
@@ -3,41 +3,40 @@ from typing import List
 import numpy as np
 
 
-def reduce_rows_cols_in_matrix(primary_matrix: np.ndarray) -> np.ndarray:
-    rows_number = primary_matrix.shape[0]
-    cols_number = primary_matrix.shape[1]
+def reduce_rows_cols_in_matrix(matrix: np.ndarray) -> np.ndarray:
+    rows_number = matrix.shape[0]
+    cols_number = matrix.shape[1]
 
-    row_numbers_to_delete = get_row_numbers_to_delete(primary_matrix, rows_number)
-    cols_numbers_to_delete = get_cols_numbers_to_delete(primary_matrix, cols_number)
+    row_numbers_to_delete = get_row_numbers_to_delete(matrix, rows_number)
+    cols_numbers_to_delete = get_cols_numbers_to_delete(matrix, cols_number)
 
-    primary_matrix = np.delete(primary_matrix, row_numbers_to_delete, axis=0)
-    primary_matrix = np.delete(primary_matrix, cols_numbers_to_delete, axis=1)
+    matrix = np.delete(matrix, row_numbers_to_delete, axis=0)
+    matrix = np.delete(matrix, cols_numbers_to_delete, axis=1)
 
-    return primary_matrix
+    return matrix
 
 
-def get_row_numbers_to_delete(primary_matrix: np.ndarray, rows_number: int) -> List[int]:
+def get_row_numbers_to_delete(matrix: np.ndarray, rows_number: int) -> List[int]:
     row_numbers_to_delete: List[int] = []
 
     for i in range(rows_number):
         for j in range(rows_number):
             if i != j:
-                if (primary_matrix[i] <= primary_matrix[j]).all() \
-                        and has_smaller_item_in_row(primary_matrix[i], primary_matrix[j]):
+                if (matrix[i] <= matrix[j]).all() \
+                        and has_smaller_item_in_row(matrix[i], matrix[j]):
                     row_numbers_to_delete.append(i)
 
     return row_numbers_to_delete
 
 
-def get_cols_numbers_to_delete(primary_matrix: np.ndarray, cols_number: int) -> List[int]:
+def get_cols_numbers_to_delete(matrix: np.ndarray, cols_number: int) -> List[int]:
     cols_numbers_to_delete: List[int] = []
 
     for i in range(cols_number):
         for j in range(cols_number):
             if i != j:
-                if (primary_matrix[:, i] >= primary_matrix[:, j]).all() \
-                        and has_bigger_item_in_column(primary_matrix[:, i],
-                                                      primary_matrix[:, j]):
+                if (matrix[:, i] >= matrix[:, j]).all() \
+                        and has_bigger_item_in_column(matrix[:, i], matrix[:, j]):
                     cols_numbers_to_delete.append(i)
 
     return cols_numbers_to_delete
