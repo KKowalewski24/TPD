6e580e727de3e9e276872b8239e91e0fe56b3a33
Kamil Kowalewski <szamil24@gmail.com>
2021-03-31 12:34:32 +0200

    Add part of pulp + add reverting game value

--- Task2/module/linear_programming_solution.py
+++ Task2/module/linear_programming_solution.py
@@ -1,11 +1,23 @@
 from typing import Tuple
 
 import numpy as np
-from pulp import LpVariable, LpProblem, LpMinimize, LpMaximize
+from pulp import LpMaximize, LpMinimize, LpProblem, LpVariable, lpSum
 
 
 def get_linear_solution(matrix: np.ndarray) -> None:
     scaled_matrix, scale_value = scale_matrix(matrix)
+
+    variable_x = LpVariable("X", lowBound=0)
+    problem_a = LpProblem("A", LpMinimize)
+    problem_a += lpSum(variable_x)
+
+    variable_y = LpVariable("Y", lowBound=0)
+    problem_b = LpProblem("B", LpMaximize)
+    problem_b += lpSum(variable_y)
+
+    # TODO CHANGE FOR REAL VALUE
+    game_value = 1
+    revert_scaling_game_value(game_value, scale_value)
 
 
 def scale_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, float]:
@@ -13,4 +25,10 @@ def scale_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, float]:
     if min_value_in_matrix <= 0:
         matrix = matrix + abs(min_value_in_matrix)
 
-    return matrix, abs(min_value_in_matrix)
+    return matrix, min_value_in_matrix
+
+
+def revert_scaling_game_value(game_value: float, scale_value: float) -> float:
+    if scale_value <= 0:
+        game_value = game_value - abs(scale_value)
+    return game_value
