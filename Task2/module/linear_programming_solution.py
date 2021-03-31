aeee6bbe0791a2d39065732188e971fa276004ce
Kamil Kowalewski <szamil24@gmail.com>
2021-03-31 16:20:05 +0200

    Refactor to functions

--- Task2/module/linear_programming_solution.py
+++ Task2/module/linear_programming_solution.py
@@ -9,37 +9,47 @@ def get_linear_solution(matrix: np.ndarray) -> Tuple[List[float], List[float], f
     rows_number = scaled_matrix.shape[0]
     cols_number = scaled_matrix.shape[1]
 
-    problem_a = LpProblem("A", LpMinimize)
-    x_variables = [LpVariable("X" + str(index + 1), 0) for index in range(rows_number)]
-    problem_a += lpSum(x_variables)
+    a_problem_variables = minimize_by_columns(scaled_matrix, rows_number, cols_number)
+    b_problem_variables = maximize_by_rows(scaled_matrix, rows_number, cols_number)
 
-    for i in range(cols_number):
-        problem_a += lpSum(
-            [x_variables[j] * scaled_matrix[j, i] for j in
-             range(len(scaled_matrix[:, i]))]
-        ) >= 1
-
-    problem_a.solve()
-
-    problem_b = LpProblem("B", LpMaximize)
-    y_variables = [LpVariable("Y" + str(index + 1), 0) for index in range(cols_number)]
-    problem_b += lpSum(y_variables)
-
-    for i in range(rows_number):
-        problem_b += lpSum(
-            [y_variables[j] * scaled_matrix[i, j] for j in range(len(scaled_matrix[i]))]
-        ) <= 1
-
-    problem_b.solve()
-
-    game_value = 1 / sum([var.varValue for var in problem_a.variables()])
+    game_value = 1 / sum(a_problem_variables)
     reverted_game_value = round(revert_scaling_game_value(game_value, scale_value), 2)
-    player_a = [round(var.varValue * reverted_game_value, 2) for var in
-                problem_a.variables()]
-    player_b = [round(var.varValue * reverted_game_value, 2) for var in
-                problem_b.variables()]
+    player_a = [round(var * reverted_game_value, 2) for var in a_problem_variables]
+    player_b = [round(var * reverted_game_value, 2) for var in b_problem_variables]
 
     return player_a, player_b, reverted_game_value
+
+
+def minimize_by_columns(matrix: np.ndarray, rows_number: int,
+                        cols_number: int) -> List[float]:
+    problem = LpProblem("A", LpMinimize)
+    variables = [LpVariable("X" + str(index + 1), 0) for index in range(rows_number)]
+    problem += lpSum(variables)
+
+    for i in range(cols_number):
+        problem += lpSum(
+            [variables[j] * matrix[j, i] for j in range(len(matrix[:, i]))]
+        ) >= 1
+
+    problem.solve()
+
+    return [var.varValue for var in problem.variables()]
+
+
+def maximize_by_rows(matrix: np.ndarray, rows_number: int,
+                     cols_number: int) -> List[float]:
+    problem = LpProblem("B", LpMaximize)
+    variables = [LpVariable("Y" + str(index + 1), 0) for index in range(cols_number)]
+    problem += lpSum(variables)
+
+    for i in range(rows_number):
+        problem += lpSum(
+            [variables[j] * matrix[i, j] for j in range(len(matrix[i]))]
+        ) <= 1
+
+    problem.solve()
+
+    return [var.varValue for var in problem.variables()]
 
 
 def scale_matrix(matrix: np.ndarray) -> Tuple[np.ndarray, float]:
