fcba9f2eaf4b75b09f2756cb81ab0b6a9ee6b010
Kamil Kowalewski <szamil24@gmail.com>
2021-04-13 18:55:51 +0200

    REMOVE NUMPY APPROACH - IF NEEDED GO HERE

--- Task3/module/reader.py
+++ Task3/module/reader.py
@@ -1,6 +1,5 @@
 from typing import Dict, List
 
-import numpy as np
 import pandas as pd
 
 
@@ -8,14 +7,5 @@ def read_csv_matrices(filenames: List[str]) -> Dict[int, pd.DataFrame]:
     matrices: Dict[int, pd.DataFrame] = {}
     for index in range(len(filenames)):
         matrices[index] = pd.read_csv(filenames[index])
-
-    return matrices
-
-
-# TODO PROBABLY DELETE THIS
-def read_matrices(filenames: List[str]) -> Dict[int, np.ndarray]:
-    matrices: Dict[int, np.ndarray] = {}
-    for index in range(len(filenames)):
-        matrices[index] = np.loadtxt(filenames[index])
 
     return matrices
