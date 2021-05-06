83777a0a74115c81a67b953a80f3e2859b50c10b
Kamil Kowalewski <szamil24@gmail.com>
2021-05-06 19:45:58 +0200

    Add process_table_from_lecture.csv + stage in graph

--- Task4/module/dynamic_programming_solution.py
+++ Task4/module/dynamic_programming_solution.py
@@ -13,14 +13,15 @@ def find_optimal_strategy(process_table: pd.DataFrame) -> Tuple[List[str], float
 def _build_graph(process_table: pd.DataFrame) -> Dict:
     graph = {}
     for index, row in process_table.iterrows():
-        initial_state, end_state, losses = (
-            int(row["initial_state"]), int(row["end_state"]), float(row["losses"])
+        stage, initial_state, end_state, losses = (
+            int(row["stage"]), int(row["initial_state"]),
+            int(row["end_state"]), float(row["losses"])
         )
 
         if initial_state not in graph:
-            graph[initial_state] = {"before": {}, "after": {}}
+            graph[initial_state] = {"before": {}, "after": {}, "stage": stage}
         if end_state not in graph:
-            graph[end_state] = {"before": {}, "after": {}}
+            graph[end_state] = {"before": {}, "after": {}, "stage": stage}
 
         graph[initial_state]["after"][end_state] = losses
         graph[end_state]["before"][initial_state] = losses
