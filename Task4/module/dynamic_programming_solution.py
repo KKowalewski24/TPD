a72ecc4e58788a384f6c2bdd95c46f3e47729d68
Kamil Kowalewski <szamil24@gmail.com>
2021-05-10 10:01:33 +0200

    Add min losses calculation

--- Task4/module/dynamic_programming_solution.py
+++ Task4/module/dynamic_programming_solution.py
@@ -5,27 +5,19 @@ import pandas as pd
 
 def find_optimal_strategy(process_table: pd.DataFrame) -> Tuple[List[str], float]:
     graph = _build_graph(process_table)
-    stages = _get_stages_number(process_table)
+    stages = _get_stages_numbers(process_table)
 
     for stage in reversed(stages):
-        for node in reversed(graph):
-            if graph[node]["stage"] == stage:
-                before_items = graph[node]["before"]
-                after_items = _get_dict_when_no_neighbours(graph[node]["after"], node)
-                for after in after_items:
-                    print(after, end=" ")
-                    print(after_items[after])
+        for node in [node for node in reversed(graph) if graph[node]["stage"] == stage]:
+            if len(graph[node]["after"]) > 0:
+                graph[node]["min_losses"] = min(
+                    [graph[node]["after"][after] + graph[after]["min_losses"]
+                     for after in graph[node]["after"]]
+                )
+            else:
+                graph[node]["min_losses"] = 0
 
     return [""], 1
-
-
-# Returns passed dict if it is not empty, otherwise it returns new dict with
-# node_number and ZERO value of loss
-def _get_dict_when_no_neighbours(neighbours: Dict[int, float],
-                                 node_number: int) -> Dict[int, float]:
-    if bool(neighbours):
-        return neighbours
-    return {node_number: 0}
 
 
 def _build_graph(process_table: pd.DataFrame) -> Dict:
@@ -41,11 +33,12 @@ def _build_graph(process_table: pd.DataFrame) -> Dict:
         if end_state not in graph:
             graph[end_state] = {"before": {}, "after": {}, "stage": stage}
 
+        graph[end_state]["stage"] = stage
         graph[initial_state]["after"][end_state] = losses
         graph[end_state]["before"][initial_state] = losses
 
     return graph
 
 
-def _get_stages_number(process_table: pd.DataFrame) -> List[int]:
-    return process_table["stage"].unique()
+def _get_stages_numbers(process_table: pd.DataFrame) -> List[int]:
+    return sorted(process_table["stage"].unique())
