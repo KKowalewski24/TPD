309283389fb1f396f0a83ae4d03ba402f8679375
Jan Karwowski <karwojan@tlen.pl>
2021-06-12 11:18:24 +0200

    Implement (not working) selfish algorithm

--- Task5/main.py
+++ Task5/main.py
@@ -1,44 +1,73 @@
-import subprocess
-import sys
-
-"""
-"""
+from random import expovariate
 
 
-# VAR ------------------------------------------------------------------------ #
+class Task:
+    def __init__(self, start_time, handling_time):
+        self.start_time = start_time
+        self.handling_time = handling_time
+        self.end_time = None
+        self.consumed_time = 0
+        self.priority = 0
 
-# MAIN ----------------------------------------------------------------------- #
-def main() -> None:
-    display_finish()
+    def handle(self, current_time, spent_time):
+        self.consumed_time += spent_time
+        if self.consumed_time >= self.handling_time:
+            self.end_time = current_time
 
-
-# DEF ------------------------------------------------------------------------ #
-
-# UTIL ----------------------------------------------------------------------- #
-def check_types_check_style() -> None:
-    subprocess.call(["mypy", "."])
-    subprocess.call(["flake8", "."])
-
-
-def compile_to_pyc() -> None:
-    subprocess.call(["python", "-m", "compileall", "."])
+    def update_priority(self, current_time, coef):
+        self.priority = (current_time - self.start_time) * coef
 
 
-def check_if_exists_in_args(arg: str) -> bool:
-    return arg in sys.argv
+
+def generate_tasks(mean_break_time, mean_handling_time, number_of_tasks):
+    tasks = [] 
+    last_start_time = 0.0
+    for i in range(number_of_tasks):
+        start_time = last_start_time + expovariate(1.0 / mean_break_time)
+        handling_time = expovariate(1.0 / mean_handling_time)
+        tasks.append(Task(start_time, handling_time))
+        last_start_time = start_time
+    return tasks
 
 
-def display_finish() -> None:
-    print("------------------------------------------------------------------------")
-    print("FINISHED")
-    print("------------------------------------------------------------------------")
+# parameters
+alpha = 1
+beta = 1
+mean_break_time = 3
+mean_handling_time = 5
 
+# lists of tasks
+tasks = generate_tasks(mean_break_time, mean_handling_time, int(input("Number of tasks:")))
+waiting_tasks = []
+handled_tasks = []
+done_tasks = []
 
-# __MAIN__ ------------------------------------------------------------------- #
-if __name__ == "__main__":
-    if check_if_exists_in_args("-t"):
-        check_types_check_style()
-    elif check_if_exists_in_args("-b"):
-        compile_to_pyc()
-    else:
-        main()
+# main loop (1 loop equals 1 second)
+time = 0
+DELTA = 1
+while tasks[-1].end_time is None:
+    time += DELTA
+
+    # add new tasks to 'waiting'
+    waiting_tasks += [task for task in tasks if task.start_time >= time]
+
+    # update priorities
+    for task in waiting_tasks:
+        task.update_priority(time, alpha)
+    for task in handled_tasks:
+        task.update_priority(time, beta)
+
+    # move tasks from 'waiting' to 'handled'
+    min_handled_priority = min((task.priority for task in handled_tasks), default=0)
+    handled_tasks += [task for task in waiting_tasks if task.priority >= min_handled_priority]
+    waiting_tasks = [task for task in waiting_tasks if task.priority < min_handled_priority]
+
+    # handle tasks (round robin)
+    for task in handled_tasks:
+        task.handle(time, DELTA / len(handled_tasks))
+
+    # move tasks from 'handled' to 'done'
+    done_tasks += [task for task in handled_tasks if task.end_time is not None]
+    handled_tasks = [task for task in handled_tasks if task.end_time is None]
+
+    print("{}: {}/{}".format(time, len(done_tasks), len(tasks)))
